ec46e90af2d0db0c03dabc241aca178cc1f5936d
smin9408 <stmdaks888@naver.com>
2022-02-17 17:01:41 +0900

    상세페이지에 데이터 전해주기 완료

--- app/src/main/java/com/neppplus/jickbangclone/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangclone/ViewRoomDetailActivity.kt
@@ -2,7 +2,11 @@ package com.neppplus.jickbangclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangclone.datas.RoomData
 import kotlinx.android.synthetic.main.activity_view_room_detail.*
+import kotlinx.android.synthetic.main.activity_view_room_detail.txtDescription
+import kotlinx.android.synthetic.main.activity_view_room_detail.txtPrice
+import kotlinx.android.synthetic.main.room_list_item.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +14,14 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_detail)
 
 
-        val price = intent.getStringExtra("priceInfo")
+        val roomData = intent.getSerializableExtra("roomInfo") as RoomData
 
-        txtPrice.text = price
+        txtPrice.text = roomData.getFormattedPrice()
+        txtDescription.text = roomData.description
+
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
+
 
     }
 }
